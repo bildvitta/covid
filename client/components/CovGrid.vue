136ecfc49f68a2f4461eb1ba0a5f3fac121fb026
Douglas Calora <douglascalora123@gmail.com>
2020-05-15 15:50:12 -0300

    implementing select and grid

---
+++ client/components/CovGrid.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="cov-grid">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss">
+$grid-gutter: 25px !default;
+
+// ==========================================================================
+// Grid
+// ==========================================================================
+.cov-grid {
+  $root: &;
+
+  box-sizing: border-box;
+  display: flex;
+  flex-flow: row wrap;
+  margin: 0;
+  padding: 0;
+
+  > * {
+    box-sizing: inherit;
+    flex-basis: 100%;
+    min-width: 0;
+  }
+
+  &--align-center {
+    justify-content: center;
+  }
+
+  &--align-right {
+    justify-content: flex-end;
+  }
+
+  &--align-middle {
+    align-items: center;
+  }
+
+  &--align-bottom {
+    align-items: flex-end;
+  }
+
+  &--fill > * {
+    flex: 1 1 0%;
+  }
+
+  &--fit > * {
+    flex-basis: auto;
+  }
+
+  &--equal-height > * {
+    display: flex;
+  }
+
+  &--with-gutter {
+    margin: #{ -0.5 * $grid-gutter} #{ -0.5 * $grid-gutter};
+
+    > * {
+      padding: #{ 0.5 * $grid-gutter} #{0.5 * $grid-gutter};
+    }
+  }
+}
+</style>
---
+++ client/components/CovGridCell.vue
@@ -0,0 +1,215 @@
+<template>
+  <div v-bind="$attrs" :class="classes">
+    <slot />
+  </div>
+</template>
+
+<script>
+const ignore = ['col']
+
+export default {
+  props: {
+    breakpoints: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+
+  computed: {
+    defaultBreakpoint () {
+      return { col: '1of6', sm: '1of6', md: '1of6', lg: '1of6', ...this.breakpoints }
+    },
+
+    classes () {
+      const classes = []
+
+      for (const key in this.defaultBreakpoint) {
+        const breakpoint = ignore.includes(key) ? '' : `${key}-`
+
+        classes.push(`${breakpoint}cov-grid-cell-size${this.defaultBreakpoint[key]}`)
+      }
+
+      return classes
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../node_modules/breakpoint-sass/stylesheets/_breakpoint';
+
+@mixin cov-grid-cell($prefix: '') {
+  .#{$prefix}size1of12 {
+    flex-basis: auto;
+    width: calc(100% / 12);
+  }
+
+  .#{$prefix}size1of10 {
+    flex-basis: auto;
+    width: 10%;
+  }
+
+  .#{$prefix}size1of8 {
+    flex-basis: auto;
+    width: 12.5%;
+  }
+
+  .#{$prefix}size1of6,
+  .#{$prefix}size2of12 {
+    flex-basis: auto;
+    width: calc(100% / 6);
+  }
+
+  .#{$prefix}size1of5,
+  .#{$prefix}size2of10 {
+    flex-basis: auto;
+    width: 20%;
+  }
+
+  .#{$prefix}size1of4,
+  .#{$prefix}size2of8,
+  .#{$prefix}size3of12 {
+    flex-basis: auto;
+    width: 25%;
+  }
+
+  .#{$prefix}size3of10 {
+    flex-basis: auto;
+    width: 30%;
+  }
+
+  .#{$prefix}size1of3,
+  .#{$prefix}size2of6,
+  .#{$prefix}size4of12 {
+    flex-basis: auto;
+    width: calc(100% / 3);
+  }
+
+  .#{$prefix}size3of8 {
+    flex-basis: auto;
+    width: 37.5%;
+  }
+
+  .#{$prefix}size2of5,
+  .#{$prefix}size4of10 {
+    flex-basis: auto;
+    width: 40%;
+  }
+
+  .#{$prefix}size5of12 {
+    flex-basis: auto;
+    width: calc(100% * 5 / 12);
+  }
+
+  .#{$prefix}size1of2,
+  .#{$prefix}size2of4,
+  .#{$prefix}size3of6,
+  .#{$prefix}size4of8,
+  .#{$prefix}size5of10,
+  .#{$prefix}size6of12 {
+    flex-basis: auto;
+    width: 50%;
+  }
+
+  .#{$prefix}size7of12 {
+    flex-basis: auto;
+    width: calc(100% * 7 / 12);
+  }
+
+  .#{$prefix}size3of5,
+  .#{$prefix}size6of10 {
+    flex-basis: auto;
+    width: 60%;
+  }
+
+  .#{$prefix}size5of8 {
+    flex-basis: auto;
+    width: 62.5%;
+  }
+
+  .#{$prefix}size2of3,
+  .#{$prefix}size4of6,
+  .#{$prefix}size8of12 {
+    flex-basis: auto;
+    width: calc(100% * 2 / 3);
+  }
+
+  .#{$prefix}size7of10 {
+    flex-basis: auto;
+    width: 70%;
+  }
+
+  .#{$prefix}size3of4,
+  .#{$prefix}size6of8,
+  .#{$prefix}size9of12 {
+    flex-basis: auto;
+    width: 75%;
+  }
+
+  .#{$prefix}size4of5,
+  .#{$prefix}size8of10 {
+    flex-basis: auto;
+    width: 80%;
+  }
+
+  .#{$prefix}size5of6,
+  .#{$prefix}size10of12 {
+    flex-basis: auto;
+    width: calc(100% * 5 / 6);
+  }
+
+  .#{$prefix}size7of8 {
+    flex-basis: auto;
+    width: 87.5%;
+  }
+
+  .#{$prefix}size9of10 {
+    flex-basis: auto;
+    width: 90%;
+  }
+
+  .#{$prefix}size11of12 {
+    flex-basis: auto;
+    width: calc(100% * 11 / 12);
+  }
+
+  .#{$prefix}sizeFit {
+    flex-basis: auto;
+  }
+
+  .#{$prefix}sizeFill {
+    flex: 1 1 0%;
+    flex-basis: 0%;
+  }
+
+  .#{$prefix}sizeFull {
+    width: 100%;
+  }
+}
+
+@include cov-grid-cell;
+
+// @include breakpoint('480px') {
+//   @include cov-grid-cell('sm-');
+// }
+
+@media screen and (max-width: 480px) {
+  @include cov-grid-cell('sm-');
+}
+
+@media screen and (max-width: 768px) {
+  @include cov-grid-cell('md-');
+}
+
+@media screen and (max-width: 1080px) {
+  @include cov-grid-cell('lg-');
+}
+
+// @include breakpoint('768px') {
+//   @include cov-grid-cell('md-');
+// }
+
+// @include breakpoint('1080px') {
+//   @include cov-grid-cell('lg-');
+// }
+</style>
