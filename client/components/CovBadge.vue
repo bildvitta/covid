3ef306fc401c64945d112c4282a0c4d14f34e1d4
Wagner Guerrero <wagner@formaweb.com.br>
2020-05-15 15:53:59 -0300

    making badge

---
+++ client/components/CovBadge.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="badge" :class="modifier">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: 'primary'
+    },
+
+    outlined: {
+      type: Boolean
+    }
+  },
+
+  computed: {
+    modifier () {
+      return `badge--${this.color}`
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .badge {
+    border-radius: 11px;
+    padding: 3px 6px;
+
+    &--secondary {
+      background-color: red;
+    }
+  }
+</style>
