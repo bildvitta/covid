88c5562de44ab4dc70a6bc058dbee45fe81fabc0
Caio Tarifa <caiotarifa@gmail.com>
2020-05-14 21:18:06 -0300

    CSS is working

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,55 +1,27 @@
 <template>
   <div>
-    <nuxt />
+    <header class="header">
+      <div class="container">
+        <div class="header__brand">
+          COVID-19
+        </div>
+
+        <div class="header__title">
+          Ocupação de leitos hospitalares
+        </div>
+      </div>
+    </header>
+
+    <main class="main">
+      <div class="container">
+        <nuxt />
+      </div>
+    </main>
+
+    <footer class="footer">
+      <div class="container">
+        Este é um projeto Open Source. Tenha <a href="?" target="_blank">acesso ao código-fonte na íntegra</a>.
+      </div>
+    </footer>
   </div>
 </template>
-
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -1,4 +1,3 @@
-
 export default {
   mode: 'universal',
   /*
@@ -6,15 +5,19 @@ export default {
   */
   head: {
     title: process.env.npm_package_name || '',
+
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
+
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;0,700;1,400;1,700&display=swap' }
     ]
   },
+
   /*
   ** Customize the progress-bar color
   */
@@ -23,6 +26,7 @@ export default {
   ** Global CSS
   */
   css: [
+    '@/assets/styles/styles.scss'
   ],
   /*
   ** Plugins to load before mounting the App
