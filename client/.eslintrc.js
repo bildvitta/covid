ba8df1207a6ceaf17d76f93c9efb4ad1e61d49c9
Caio Tarifa <caiotarifa@gmail.com>
2020-05-16 00:25:02 -0300

    eslint

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,17 +1,35 @@
 module.exports = {
   root: true,
+
   env: {
     browser: true,
     node: true
   },
+
   parserOptions: {
     parser: 'babel-eslint'
   },
+
   extends: [
     '@nuxtjs',
-    'plugin:nuxt/recommended'
+    'plugin:nuxt/recommended',
+    'plugin:vue/recommended'
   ],
-  // add your custom rules here
+
   rules: {
+    'max-len': { tabWidth: 2 },
+
+    // Recommended
+    'vue/max-attributes-per-line': 'off',
+    'vue/no-v-html': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+
+    // Uncategorized
+    'vue/array-bracket-spacing': ['error', 'never'],
+    'vue/arrow-spacing': ['error', { before: true, after: false }],
+    'vue/attributes-order': ['error', { alphabetical: true }],
+    'vue/block-spacing': ['error', 'always'],
+    'vue/no-reserved-component-names': ['error'],
+    'vue/script-indent': ['error']
   }
 }
