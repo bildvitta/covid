9a706eab4a0ac0a769a2ea3b66e4131d2c70d250
Wagner Guerrero <wagner@formaweb.com.br>
2020-05-21 20:24:47 -0300

    config host

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -1,6 +1,11 @@
 export default {
   mode: 'universal',
 
+  server: {
+    port: 8080, // default: 3000
+    host: '0.0.0.0' // default: localhost
+  },
+
   head: {
     title: 'COVID-19 | Ocupação de leitos hospitalares',
 
