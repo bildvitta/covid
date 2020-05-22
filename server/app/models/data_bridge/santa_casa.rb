4decaf9c931204c2622db211d474708811ea0abc
Hugo Demiglio <hugo@formaweb.com.br>
2020-05-22 13:00:48 -0300

    Oracle ok

---
+++ server/app/models/data_bridge/santa_casa.rb
@@ -0,0 +1,37 @@
+module DataBridge
+  class SantaCasa < DataBridge::OracleBase
+
+    def connection_data
+      Rails.application.credentials.santa_casa_connection
+    end
+
+    def get_data
+      self.load_by_query('SELECT * FROM covid')
+      process_beds
+
+      # self.close_connection!
+
+      self.data = nil if Rails.env.production?
+      return self
+    end
+
+    protected
+
+    def process_beds
+      self.results = []
+      puts self.data.inspect
+      return unless self.valid_data?
+
+      self.data.each do |r|
+        self.results << DataBridge::InternalObject.new(
+          hospital_slug:    'santa-casa-ribeirao',
+          status:           get_status(r[3]),
+          bed_type:         get_bed_type(r[2]),
+          slug:             r[1].parameterize,
+          using_ventilator: r[4].to_s.downcase == 'true',
+        )
+      end
+    end
+
+  end
+end
