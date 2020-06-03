516e25e5d576df179a63d9a5322b45ed9be03934
Hugo Demiglio <hugo@formaweb.com.br>
2020-06-03 11:56:45 -0300

    Fixing routines

--- server/config/schedule.rb
+++ server/config/schedule.rb
@@ -4,6 +4,10 @@ every 1.day, at: '0:30' do
   runner 'Routines::GenerateHistorical.new(1.day.ago.to_date).run!'
 end
 
+every 1.day, at: '1:30' do
+  runner 'CovidCase.populate_with_api'
+end
+
 every 1.day, at: ['0:05', '6:05', '13:05', '19:05'] do
   runner "DataBridge::Unimed.new.get_data.save!"
 end
@@ -15,3 +19,7 @@ end
 every 1.day, at: ['0:10', '6:10', '13:10', '19:10'] do
   runner "DataBridge::SantaCasa.new.get_data.save!"
 end
+
+
+# undefined method `children' for nil:NilClass
+# DataBridge::Unimed.new.get_data.save!; DataBridge::SaoLucas.new.get_data.save!; DataBridge::SantaCasa.new.get_data.save!; Routines::GenerateHistorical.new(1.day.ago.to_date).run!
