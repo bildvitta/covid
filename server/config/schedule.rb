6a79d1b700b9ecb0734cac7ca16e67b41471498d
Hugo Demiglio <hugodemiglio@MacBook-Pro-de-Hugo.local>
2020-06-08 18:46:25 -0300

    Changing routines

--- server/config/schedule.rb
+++ server/config/schedule.rb
@@ -4,7 +4,7 @@ every 1.day, at: '0:30' do
   runner 'Routines::GenerateHistorical.new(1.day.ago.to_date).run!'
 end
 
-every :hour do
+every 1.day, at: '18:00' do
   runner 'CovidCase.populate_with_api'
 end
 
@@ -12,7 +12,7 @@ every 1.day, at: ['0:05', '6:05', '13:05', '19:05'] do
   runner "DataBridge::Unimed.new.get_data.save!"
 end
 
-every 1.day, at: ['0:07', '6:07', '13:07', '19:07'] do
+every :hour do
   runner "DataBridge::SaoLucas.new.get_data.save!"
 end
 
